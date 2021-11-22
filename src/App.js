6fdd9f4be724df1691b69d31023026c6d21f1177
chirag0320 <90890056+chirag0320@users.noreply.github.com>
2021-11-21 23:06:15 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Home from './pages/Home.js';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {HashRouter as Router,Routes,Route} from 'react-router-dom'
 import ImageGallaryComponent from './pages/image-gallery.component';
 import Newtry from './pages/Newtry.js';
 
